a0eafc131892921995607d51a733c8083d32945e
Abdurrahman Shidiq <a.rahmanshidiq@gmail.com>
2022-05-18 18:52:01 +0700

    learn dbt

---
+++ models/fct/fct_reviews.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema = 'fail'
+    )
+}}
+
+WITH src_reviews AS (
+    SELECT 
+        *
+    FROM {{ ref('src_reviews') }}
+)
+SELECT
+    {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,
+    *
+FROM
+    src_reviews
+WHERE review_text IS NOT NULL
+--  conditions on how to increment ( tell dbt what are the new records)
+{% if is_incremental() %}
+    AND review_date > (SELECT max(review_date) FROM {{ this }})
+{% endif %}
